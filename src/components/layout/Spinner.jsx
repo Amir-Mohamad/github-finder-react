313a5550b8ad3bb645018facb82b3da2542bc9a0
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2022-07-19 11:31:01 +0430

    feat: add fetchUsers and loading componenets

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -3,7 +3,7 @@ import spinner from "./assets/spinner.gif";
 function Spinner() {
 	return (
 		<div className="w-100 mt-20">
-			<img className="text-center mx-auto" src={spinner} alt="Loading ..." />
+			<img className="text-center mx-auto" src={spinner} alt="Loading..." />
 		</div>
 	);
 }
