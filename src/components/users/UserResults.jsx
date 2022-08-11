4130a946dd2b60503b8d7b49385240dbcf8b103a
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2022-08-11 13:44:56 +0430

    feat: add useReducers, useContext & clearUsers, SearchUsers

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-	const [users, setUsers] = useState([]);
-	const [loading, setLoading] = useState(true);
+	const { users, loading } = useContext(GithubContext);
 
-	useEffect(() => {
-		fetchUsers();
-	}, []);
-
-	const fetchUsers = async () => {
-		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-			headers: {
-				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-			},
-		});
-		const data = await response.json();
-		setUsers(data);
-		setLoading(false);
-	};
 	if (!loading) {
 		return (
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import UserResults from "../components/users/UserResults";
 import { useEffect, useState } from "react";
+import UserSearch from "../components/users/UserSearch";
 
 function Home() {
 	return (
 		<div>
+			<UserSearch />
 			<UserResults />
 		</div>
 	);
