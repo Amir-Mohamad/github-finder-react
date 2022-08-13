0f5180954810314ebfeefedf7ace9bf3ce5980e7
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2022-08-13 13:05:36 +0430

    feat: add get repos & single user

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 	};
 	const [state, dispatch] = useReducer(githubReducer, initialState);
@@ -49,6 +50,29 @@ export const GithubProvider = ({ children }) => {
 			});
 		}
 	};
+	// get user repos
+	const getUserRepos = async (login) => {
+		setLoading();
+		const params = new URLSearchParams({
+			sort: "created",
+			per_page: 10,
+		});
+
+		const response = await fetch(
+			`${GITHUB_URL}/users/${login}/repos?${params}`,
+			{
+				headers: {
+					Authorization: `token ${GITHUB_TOKEN}`,
+				},
+			}
+		);
+		const data = await response.json();
+		dispatch({
+			type: "GET_REPOS",
+			payload: data,
+		});
+	};
+
 	const clearUsers = () => {
 		dispatch({ type: "CLEAR_USERS" });
 	};
@@ -60,9 +84,11 @@ export const GithubProvider = ({ children }) => {
 				users: state.users,
 				user: state.user,
 				loading: state.loading,
+				repos: state.repos,
 				searchUsers,
 				clearUsers,
 				getUser,
+				getUserRepos,
 			}}
 		>
 			{children}
